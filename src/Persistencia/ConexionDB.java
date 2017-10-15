0238ab41ca9e06d62afeea8fb53a5779cbc6a521
Kevin Monzon <kevineloy11@gmail.com>
2017-10-15 19:17:02 -0300

    conexion a bd con archivo properties

--- src/Persistencia/ConexionDB.java
+++ src/Persistencia/ConexionDB.java
@@ -5,10 +5,15 @@
  */
 package Persistencia;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,12 +28,6 @@ public class ConexionDB {
     private final String user="root";
     private final String pass="root";*/
     
-    private static String host="localhost";
-    private static String port="3306";
-    private static String db="espotify";
-    private static String user="root";
-    private static String pass="root";
-    
     //Para hacer ConexionDB singleton descomentar
     private static Connection conexion=null;
     //private Connection conexion=null;
@@ -40,11 +39,25 @@ public class ConexionDB {
     //public Connection getConexion() {
         if (conexion == null) {
             try {
+                Properties p = new Properties();
+                InputStream is = new FileInputStream("configuraciones.properties");
+                p.load(is);
+                
+                String host = p.getProperty("host");
+                String port = p.getProperty("port");
+                String db = p.getProperty("db");
+                String user = p.getProperty("user");
+                String pass = p.getProperty("pass");
+                
                 Driver driver = new com.mysql.jdbc.Driver();
                 DriverManager.registerDriver(driver);
                 conexion = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db, user, pass);
             } catch (SQLException ex) {
                 ex.printStackTrace();
+                Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
                 Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
